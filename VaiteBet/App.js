c458dcbd0b536205b9fda04ba1f14a44c6b5cc8f
Guilherme Restelatto Zanella <96738204+guizanella@users.noreply.github.com>
2023-09-17 17:58:16 -0300

    ajustes tela inicial

--- VaiteBet/App.js
+++ VaiteBet/App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+
+    render(){
+
+        const styles = StyleSheet.create({
+            container: {
+                flex: 1,
+                backgroundColor: '#000',
+                alignItems: 'center',
+                justifyContent: 'center',
+            },
+        });
+
+        return (
+            <View style={styles.container}>
+                <Text style={{color: "yellow"}}>VaiteBet</Text>
+                <Text style={{color: "yellow"}}>fala dele!!</Text>
+                <StatusBar style="auto" />
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
