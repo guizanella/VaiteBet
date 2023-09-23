cbb57db77ec4c8da0653f385beeb58e8cdcbdee3
Guilherme Restelatto Zanella <96738204+guizanella@users.noreply.github.com>
2023-09-22 21:58:03 -0300

    logo e botão tela inicial

--- VaiteBet/App.js
+++ VaiteBet/App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import React, { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+
+    render(){
+
+        const styles = StyleSheet.create({
+            container: {
+                flex: 1,
+                backgroundColor: '#000',
+                alignItems: 'center',
+                paddingTop: 80,
+            },
+        });
+
+        return (
+            <View style={styles.container}>
+                <Logo size={270}/>
+                <Text style={{color: '#FFEB3B'}}>VaiteBet</Text>
+                <Text style={{color: '#FFEB3B', paddingBottom: 50}}>fala dele!!</Text>
+                <Botao largura={200} altura={100}/>
+                <StatusBar style="auto"/>
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+class Logo extends Component{
+
+    render(){
+        return (
+            <Image
+                source={require('./imagens/logo_transparent.png')}
+                style={{width: this.props.size, height: this.props.size}}
+            />
+        );
+    }
+}
+
+class Botao extends Component{
+
+    render(){
+        return (
+            <Button
+                title="Entrar"
+                color='#FFEB3B'
+            />
+        );
+    }
+}
